2caf250ddafcc04f5707199a14e25d5021c9c0dc
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-05-03 23:38:18 +0800

    [Flutter Bloc workflow] - added OnClickLogin event

--- lib/bloc/profile_event.dart
+++ lib/bloc/profile_event.dart
@@ -3,3 +3,13 @@ part of 'profile_bloc.dart';
 abstract class ProfileEvent extends Equatable {
   const ProfileEvent();
 }
+
+class OnClickLogin extends ProfileEvent {
+  final String username;
+  final String password;
+
+  OnClickLogin(this.username, this.password);
+  @override
+  // TODO: implement props
+  List<Object> get props => [username, password];
+}
