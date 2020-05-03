e23b11f5e05c37f03bc9875177cdd639484c4fb2
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-05-03 22:21:12 +0800

    [Flutter Bloc workflow] - added return values for operation event

--- lib/bloc/operation_event.dart
+++ lib/bloc/operation_event.dart
@@ -5,25 +5,41 @@ abstract class OperationEvent extends Equatable {
 }
 
 class OnClickAdd extends OperationEvent {
+  final double firstNumber;
+  final double secondNumber;
+
+  OnClickAdd(this.firstNumber, this.secondNumber);
   @override
   // TODO: implement props
-  List<Object> get props => null;
+  List<Object> get props => [firstNumber, secondNumber];
 }
 
 class OnClickMinus extends OperationEvent {
+  final double firstNumber;
+  final double secondNumber;
+
+  OnClickMinus(this.firstNumber, this.secondNumber);
   @override
   // TODO: implement props
-  List<Object> get props => null;
+  List<Object> get props => [firstNumber, secondNumber];
 }
 
 class OnClickMultiply extends OperationEvent {
+  final double firstNumber;
+  final double secondNumber;
+
+  OnClickMultiply(this.firstNumber, this.secondNumber);
   @override
   // TODO: implement props
-  List<Object> get props => null;
+  List<Object> get props => [firstNumber, secondNumber];
 }
 
 class OnClickDivide extends OperationEvent {
+  final double firstNumber;
+  final double secondNumber;
+
+  OnClickDivide(this.firstNumber, this.secondNumber);
   @override
   // TODO: implement props
-  List<Object> get props => null;
+  List<Object> get props => [firstNumber, secondNumber];
 }
