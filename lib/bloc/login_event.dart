79ac6748654b3cc3c3f564e7014a9658bd7f252c
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-05-03 23:49:45 +0800

    [Flutter Bloc workflow] - Added login event

--- lib/bloc/login_event.dart
+++ lib/bloc/login_event.dart
@@ -3,3 +3,13 @@ part of 'login_bloc.dart';
 abstract class LoginEvent extends Equatable {
   const LoginEvent();
 }
+
+class OnClickLogin extends LoginEvent {
+  final String username;
+  final String password;
+
+  OnClickLogin(this.username, this.password);
+  @override
+  // TODO: implement props
+  List<Object> get props => [username, password];
+}
