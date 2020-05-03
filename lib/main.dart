5a80c0e141026173b5601c53143b90eadf833695
exodiajubel <exodiajubel@gmail.com>
2020-05-03 21:57:45 +0800

    [Flutter Bloc Workflow] - Updated main & home page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:blocworkflow/page/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/operation_bloc.dart';
+
 void main() => runApp(MaterialApp(initialRoute: '/', routes: {
-      '/': (context) => Home(),
+      '/': (context) {
+        return BlocProvider(
+          create: (context) => OperationBloc(),
+          child: Home(),
+        );
+      }
     }));
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,6 @@
+import 'package:blocworkflow/bloc/operation_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,12 +11,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo Home Page'),
-        ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: BlocConsumer<OperationBloc, OperationState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          if (state is OperationInitial) {
+            return buildOperationInitial(context);
+          } else if (state is OperationRefresh) {
+            return buildOperationRefresh(context, state.result);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget buildOperationInitial(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
@@ -24,36 +42,108 @@ class _HomeState extends State<Home> {
                 '0',
                 style: Theme.of(context).textTheme.display1,
               ),
-              SizedBox(
-                height: 15,
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   FloatingActionButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      onTriggerAdd(context);
+                    },
                     tooltip: 'Increment',
                     child: Icon(Icons.add),
                   ),
                   FloatingActionButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      onTriggerMinus(context);
+                    },
                     tooltip: 'Decrement',
                     child: Icon(Icons.remove),
                   ),
                   FloatingActionButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      onTriggerMultiply(context);
+                    },
                     tooltip: 'Multiply',
                     child: Text('X'),
                   ),
                   FloatingActionButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      onTriggerDivide(context);
+                    },
                     tooltip: 'Divide',
                     child: Text('/'),
                   ),
                 ],
               ),
-            ],
-          ),
-        ));
+            ]));
+  }
+
+  Widget buildOperationRefresh(BuildContext context, double result) {
+    return Container(
+        alignment: Alignment.center,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$result',
+                style: Theme.of(context).textTheme.display1,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: () {
+                      onTriggerAdd(context);
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      onTriggerMinus(context);
+                    },
+                    tooltip: 'Decrement',
+                    child: Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      onTriggerMultiply(context);
+                    },
+                    tooltip: 'Multiply',
+                    child: Text('X'),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      onTriggerDivide(context);
+                    },
+                    tooltip: 'Divide',
+                    child: Text('/'),
+                  ),
+                ],
+              ),
+            ]));
+  }
+
+  void onTriggerAdd(BuildContext context) {
+    final operationBloc = BlocProvider.of<OperationBloc>(context);
+    operationBloc.add(OnClickAdd());
+  }
+
+  void onTriggerMinus(BuildContext context) {
+    final operationBloc = BlocProvider.of<OperationBloc>(context);
+    operationBloc.add(OnClickMinus());
+  }
+
+  void onTriggerMultiply(BuildContext context) {
+    final operationBloc = BlocProvider.of<OperationBloc>(context);
+    operationBloc.add(OnClickMultiply());
+  }
+
+  void onTriggerDivide(BuildContext context) {
+    final operationBloc = BlocProvider.of<OperationBloc>(context);
+    operationBloc.add(OnClickDivide());
   }
 }
