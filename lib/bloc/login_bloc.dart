0fbb86cfd0d22d041cd1f74d1aefe4cffb7592b0
JeffBorongan <jeffborongan@gmail.com>
2020-05-04 00:24:18 +0800

    [Flutter Bloc Workflow] - Implemented random picture to choose then assigning of profile

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:bloc/bloc.dart';
+import 'package:blocworkflow/model/account.dart';
 import 'package:equatable/equatable.dart';
-
 part 'login_event.dart';
 part 'login_state.dart';
 
@@ -14,6 +14,15 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   Stream<LoginState> mapEventToState(
     LoginEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield LoginInitial();
+    if (event is OnClickLogin) {
+      Random random = Random();
+      int id = random.nextInt(1000);
+      String picture = 'https://i.picsum.photos/id/$id/250/250.jpg';
+      Account.imageURL = picture;
+      Account.username = event.username;
+      Account.password = event.password;
+      yield LoginSuccess();
+    }
   }
 }
