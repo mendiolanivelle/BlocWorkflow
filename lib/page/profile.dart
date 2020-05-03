7f0f04b17eaf2594b1c56c358fb179de01427f3a
exodiajubel <exodiajubel@gmail.com>
2020-05-04 00:25:13 +0800

    [Flutter Bloc Workflow] - Updated profile page

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -31,9 +31,84 @@ Widget buildProfileInitial(BuildContext context) {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Name: '),
-            Text('Nivelle'),
+            Text(
+              'Username: ',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              'Nivelle',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Password: ',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              'NivelleSky',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network('https://picsum.photos/id/237/250/250')
+          ],
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              color: Colors.black,
+              textColor: Colors.white,
+              disabledColor: Colors.grey,
+              disabledTextColor: Colors.black,
+              padding: EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+                left: 70,
+                right: 70,
+              ),
+              splashColor: Colors.grey,
+              onPressed: () {},
+              child: Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
           ],
         ),
       ],
