14af5cedc15efa1e74b991ff32d2e43cea30cdba
exodiajubel <exodiajubel@gmail.com>
2020-05-03 21:08:19 +0800

    [Flutter Bloc Workflow] - Updated home page

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -9,28 +9,51 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Demo Home Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Flutter Demo Home Page'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '0',
+                style: Theme.of(context).textTheme.display1,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: () {},
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    tooltip: 'Decrement',
+                    child: Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    tooltip: 'Multiply',
+                    child: Text('X'),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    tooltip: 'Divide',
+                    child: Text('/'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
