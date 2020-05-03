40b380e06993cb7dc200efbfff0e09d6e9ea95f5
JeffBorongan <jeffborongan@gmail.com>
2020-05-03 20:59:03 +0800

    [Flutter Bloc Workflow] - Implement the Operation Bloc

--- lib/bloc/operation_bloc.dart
+++ lib/bloc/operation_bloc.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
 part 'operation_event.dart';
 part 'operation_state.dart';
 
 class OperationBloc extends Bloc<OperationEvent, OperationState> {
+  double totalNumber = 0;
+
   @override
   OperationState get initialState => OperationInitial();
 
@@ -14,6 +14,19 @@ class OperationBloc extends Bloc<OperationEvent, OperationState> {
   Stream<OperationState> mapEventToState(
     OperationEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield OperationInitial();
+    if (event is OnClickAdd) {
+      totalNumber += 1.0;
+      yield OperationRefresh(totalNumber);
+    } else if (event is OnClickMinus) {
+      totalNumber -= 1.0;
+      yield OperationRefresh(totalNumber);
+    } else if (event is OnClickMinus) {
+      totalNumber *= 2.0;
+      yield OperationRefresh(totalNumber);
+    } else if (event is OnClickMinus) {
+      totalNumber /= 2.0;
+      yield OperationRefresh(totalNumber);
+    }
   }
 }
