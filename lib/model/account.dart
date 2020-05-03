868d9327ba1809259c6d3ee792caf9df34ba885c
Nivelle Mendiola <mendiolanivelle@gmail.com>
2020-05-03 23:58:05 +0800

    [Flutter Bloc workflow] - added account model

---
+++ lib/model/account.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class Account extends Equatable {
+  static String username;
+  static String password;
+  static String imageURL;
+  @override
+  // TODO: implement props
+  List<Object> get props => null;
+}
